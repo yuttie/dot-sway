1120ec2f0d07abc11bc15a185e0a7769a9772a3d
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-04-10 14:51:19 +0900

    Use cut instead of sed

--- edit-note.sh
+++ edit-note.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~/Notes
-fp=$(find . -type f -regextype egrep -regex '.*\.(md|org|rst|asciidoc|adoc|txt)$' | sed -e 's!\./!!' | rofi -dmenu -i -p 'open a note')
+fp=$(find . -type f -regextype egrep -regex '.*\.(md|org|rst|asciidoc|adoc|txt)$' | cut -b3- | rofi -dmenu -i -p 'open a note')
 if [ -n "$fp" ]; then
   ~/.local/bin/term -e $EDITOR "$fp"
 fi
