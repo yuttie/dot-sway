5c7dd3cf692e81285f93cdc3cf765e91bef631c3
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-08-27 22:26:47 +0900

    Add a script to launch sway

---
+++ sway.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+export XDG_SESSION_TYPE=wayland
+export XDG_CURRENT_DESKTOP=sway
+export XDG_CURRENT_SESSION=sway
+export LIBSEAT_BACKEND=logind
+export QT_QPA_PLATFORM=wayland
+export QT_QPA_PLATFORMTHEME=qt5ct
+export GDK_DPI_SCALE=1.5
+export QT_SCALE_FACTOR=1.5
+export MOZ_ENABLE_WAYLAND=1
+export WINIT_UNIX_BACKEND=x11
+export _JAVA_AWT_WM_NONREPARENTING=1
+
+# Input method
+export XMODIFIERS=@im=fcitx
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export QT5_IM_MODULE=fcitx
+export SDL_IM_MODULE=fcitx
+export GLFW_IM_MODULE=ibus
+
+exec sway
