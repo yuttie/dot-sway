d3188b93439aa9cc48cd768a302507b171de17c8
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-08-28 00:58:54 +0900

    Remove unused script 'edit-note.sh'

--- edit-note.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd ~/Notes
-fp=$(find . -type f -regextype egrep -regex '.*\.(md|org|rst|asciidoc|adoc|txt)$' | cut -b3- | rofi -dmenu -i -p 'open a note')
-if [ -n "$fp" ]; then
-  ~/.local/bin/term -e $EDITOR "$fp"
-fi
